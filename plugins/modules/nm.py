e23f3ecb80768f0c498b86a587c5aa1f84e0cf6b
oleg <o-le-g@inbox.ru>
2020-12-08 23:40:57 +0300

    Some progress on nm module.

--- plugins/modules/nm.py
+++ plugins/modules/nm.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python3
+from ansible.module_utils.basic import AnsibleModule
+import subprocess
 
 DOCUMENTATION = r'''
 ---
 module: nm
 short_description: Some network config delivered using GNU Network Manager.
 version_added: "0.0.1"
-description: Some network config delivered using GNU Network Manager.
+description: |
+    Some network config delivered using GNU Network Manager.
+    Why not to use command module? Mainly for better idempotency control.
 options:
   name:
-    description: >
+    description: |
         NM utility which provides the functionality.
         Partially implemented: nmcli.
     default: nmcli
     required: false
     type: str
   object:
-    description: >
+    description: |
         Object which NM utility is to operate upon.
         For example if NM utility is nmcli this would be nmcli objects.
-        Partially implemented: nmcli objects - networking
+        Partially implemented: nmcli objects - networking, wifi
     default: networking
     required: false
   state:
-    description: >
-        Desired state. Actual value will depend on the NM utility and its object.
-        Not implemented.
+    description: |
+        Desired state. Actual value will depend on NM utility and its object.
+        name: nmcli, object: networking, state: ['', on, off].
+        name: nmcli, object: wifi, state: ['', on, off].
     required: false
     type: str
+  connect:
+    description: |
+        Write me...
+    requred: false
+    type: dict
 author: oleg (@ol-eg)
 ...
 '''
@@ -34,29 +44,90 @@ EXAMPLES = r'''
 - name: Enable networking control by NM.
   oppa.all.nm:
     name: nmcli
-      object: networking
-      state: enabled
+    object: networking
+    state: enabled
 '''
 RETURN = r'''
 original_message:
   description: Parameters that were passed in.
   returned: always
-  sample: "{name: nmcli, object: networking, state: enabled}"
+  sample: 'name: nmcli, object: networking, state: enabled'
 message:
   description: The output message that this module generates.
   type: str
   returned: always
-  sample: 'nmcli.networking = enabled'
+  sample: 'nmcli networking exit code: 0, stdout: enabled'
 '''
-from ansible.module_utils.basic import AnsibleModule
-
 valid = set([
     (None, None, None),
     ('nmcli', None, None),
     ('nmcli', 'networking', None),
     ('nmcli', 'networking', 'off'),
     ('nmcli', 'networking', 'on'),
+    ('nmcli', 'wifi', None),
+    ('nmcli', 'wifi', 'off'),
+    ('nmcli', 'wifi', 'on'),
 ])
+CMD_TIMEOUT = 180
+
+
+class nmcli:
+    cmd = ['nmcli']
+    objects = ('networking', 'wifi')
+
+    def _run_command(self, cmd: list) -> tuple:
+        proc = None
+        try:
+            proc = subprocess.Popen(
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                universal_newlines=True, bufsize=1,
+            )
+            out, err = proc.communicate(timeout=CMD_TIMEOUT)
+        except OSError as e:
+            out, err = ('', str(e))
+        except subprocess.TimeoutExpired as e:
+            proc.kill()
+            out, err = proc.communicate()
+            err += str(e)
+
+        return getattr(proc, 'returncode', -9999), out + err
+
+    def _on_off(self, with_args: list, state, result: str):
+        # grab current state
+        rc, state_before = self._run_command(with_args)
+        if rc:
+            return True, False, result.format('', rc, 'stderr', state_before)
+
+        if state:
+            with_args += [state]
+
+        dontrun = (
+            not state,
+            state == 'on' and state_before.startswith('enabled'),
+            state == 'off' and state_before.startswith('disabled'),
+        )
+        if any(dontrun):
+            return False, False, result.format('', rc, 'stdout', state_before)
+
+        rc, state_after = self._run_command(with_args)
+        if rc:
+            return True, False, result.format(state, rc, 'stderr', state_after)
+        else:
+            return (
+                False,
+                state_before != state_after,
+                result.format(state, rc, 'stdout', state_after)
+            )
+
+    def networking(self, state=None) -> tuple:
+        result = 'nmcli networking {} exit code: {:d}, {}: {}'
+        with_args = nmcli.cmd + [nmcli.objects[0]]
+        return self._on_off(with_args, state, result)
+
+    def wifi(self, state=None) -> tuple:
+        result = 'nmcli radio wifi {} exit code: {:d}, {}: {}'
+        with_args = nmcli.cmd + ['radio', nmcli.objects[1]]
+        return self._on_off(with_args, state, result)
 
 
 def validate(module: AnsibleModule, result: dict):
@@ -66,7 +137,7 @@ def validate(module: AnsibleModule, result: dict):
         module.params.get('state')
     )
     if params not in valid:
-        result['message'] =  'Invalid parameters choice.'
+        result['message'] = 'Invalid parameters choice.'
         module.fail_json(msg='Failed', **result)
 
 
@@ -77,11 +148,6 @@ def set_defaults(params: dict):
     for key, default in zip(keys, defaults):
         if not params.get(key):
             params[key] = default
-
-
-class nmcli:
-    def networking(self, state: str):
-        return  True, True, 'All good'
 
 
 def run_module():
@@ -105,9 +171,9 @@ def run_module():
     result['original_message'] = f'{module.params}'
     validate(module, result)
     set_defaults(module.params)
-    print(module.params)
+    print(module.params['name'])
     util = eval(module.params['name'])()
-    func = getattr(util, module.params.get('object', 'networking'))
+    func = getattr(util, module.params['object'])
     failed, changed, result['message'] = func(module.params.get('state'))
 
     if changed:
@@ -118,8 +184,10 @@ def run_module():
 
     module.exit_json(**result)
 
+
 def main():
     run_module()
+
 
 if __name__ == '__main__':
     main()
--- plugins/modules/tests/test_nm.py
+++ plugins/modules/tests/test_nm.py
@@ -1,17 +1,26 @@
 from unittest.mock import MagicMock, create_autospec
 from ansible.module_utils.basic import AnsibleModule
-import pytest
 
 from .. import nm
+
+
+class AnsibleModuleMock(MagicMock):
+    params = dict(name='nmcli', object='networking', state='on')
+
+
+class NmcliMock(MagicMock):
+    def networking(self, state=None):
+        return True, True, 'test'
 
 
 def test_validate_valid():
     module = create_autospec(AnsibleModule)
     for tup in nm.valid:
         d = dict(name=tup[0], object=tup[1], state=tup[2])
-        module.params = {k:v for k,v in d.items() if v}
+        module.params = {k: v for k, v in d.items() if v}
         nm.validate(module, dict())
         module.fail_json.assert_not_called()
+
 
 def test_validate_invalid():
     module = create_autospec(AnsibleModule)
@@ -28,9 +37,97 @@ def test_set_defaults():
     assert params == dict(name='nmcli', object='networking')
 
 
-def test_main(monkeypatch):
-    class AnsibleModuleMock(MagicMock):
-        params = dict()
+def test_nmcli_run_command():
+    nmcli = nm.nmcli()
+    rc, msg = nmcli._run_command(['echo', 'test'])
+    assert not rc
+    assert msg == 'test\n'
 
-    monkeypatch.setattr(nm, 'AnsibleModule', AnsibleModuleMock())
+
+def test_nmcli_run_command_which_fails():
+    nmcli = nm.nmcli()
+    rc, msg = nmcli._run_command(['foo'])
+    assert rc == -9999
+    assert msg == "[Errno 2] No such file or directory: 'foo': 'foo'"
+
+
+def test_nmcli_run_command_and_timeout(monkeypatch):
+    monkeypatch.setattr(nm, 'CMD_TIMEOUT', 1)
+    nmcli = nm.nmcli()
+    rc, msg = nmcli._run_command(['sleep', '2'])
+    assert rc == -9
+    assert 'timed out' in msg
+
+
+def test_on_off_failing_state_before():
+    failed, changed, msg = nm.nmcli()._on_off(
+        ['ls', '/foo'], None, '{} {:d} {} {}'
+    )
+    assert failed
+    assert not changed
+    assert '2 stderr' in msg
+
+
+def test_on_off_current_state():
+    failed, changed, msg = nm.nmcli()._on_off(['ls'], None, '{} {:d} {} {}')
+    assert not failed
+    assert not changed
+    assert '0 stdout' in msg
+
+
+def test_on_off_against_the_same_state():
+    for current, desired in zip(('enabled', 'disabled'), ('on', 'off')):
+        failed, changed, msg = nm.nmcli()._on_off(
+            ['echo', current], desired, '{} {:d} {} {}'
+        )
+        assert not failed
+        assert not changed
+        assert '0 stdout' in msg
+
+
+def test_on_off_against_opposite_state():
+    for current, desired in zip(('enabled', 'disabled'), ('off', 'on')):
+        failed, changed, msg = nm.nmcli()._on_off(
+            ['echo', current], desired, '{} {:d} {} {}'
+        )
+        assert not failed
+        assert changed
+        assert '0 stdout' in msg
+
+
+def test_on_off_failed_to_change():
+    failed, changed, msg = nm.nmcli()._on_off(
+        ['ls', '-l'], '/foo', '{} {:d} {} {}'
+    )
+    assert failed
+    assert not changed
+    assert '2 stderr' in msg
+
+
+def test_networking(monkeypatch):
+    mock = MagicMock()
+    monkeypatch.setattr(nm.nmcli, '_on_off', mock)
+    nm.nmcli().networking('on')
+    mock.assert_called_once_with(
+        ['nmcli', 'networking'],
+        'on',
+        'nmcli networking {} exit code: {:d}, {}: {}',
+    )
+
+
+def test_wifi(monkeypatch):
+    mock = MagicMock()
+    monkeypatch.setattr(nm.nmcli, '_on_off', mock)
+    nm.nmcli().wifi('on')
+    mock.assert_called_once_with(
+        ['nmcli', 'radio', 'wifi'],
+        'on',
+        'nmcli radio wifi {} exit code: {:d}, {}: {}',
+    )
+
+
+def test_main(monkeypatch):
+    # fall through for syntax and bultins
+    monkeypatch.setattr(nm, 'AnsibleModule', AnsibleModuleMock)
+    monkeypatch.setattr(nm, 'nmcli', NmcliMock)
     nm.main()
